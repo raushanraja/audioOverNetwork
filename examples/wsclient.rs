0573f14e76402d701ae3b27e9ff458656e015d60
raushanraja <vraushanraja@gmail.com>
2025-01-26 21:02:28 +0530

    feat(audio): refactor WebSocket client to use UDP for audio streaming

--- examples/wsclient.rs
+++ examples/wsclient.rs
@@ -1,120 +1,9 @@
 use anyhow::Result;
 use cpal::traits::{HostTrait, StreamTrait};
-use futures_util::{SinkExt, StreamExt};
 use rodio::DeviceTrait;
-use std::sync::Arc;
-use tokio::sync::{
-    mpsc::{Receiver, Sender},
-    Mutex,
-};
-use tokio::time::{sleep, Duration};
-use tokio_tungstenite::connect_async;
-
-pub struct WSMessage {
-    pub message: Vec<u8>,
-}
-
-pub struct WebSocketClient {
-    url: String,
-    tx: Sender<WSMessage>,
-    rx: Arc<Mutex<Receiver<WSMessage>>>,
-}
-
-pub trait Client {
-    fn connect(&self) -> impl std::future::Future<Output = Result<()>> + Send;
-    fn reconnect(&self) -> impl std::future::Future<Output = Result<()>> + Send;
-}
-
-impl WebSocketClient {
-    pub fn new(url: String, tx: Sender<WSMessage>, rx: Arc<Mutex<Receiver<WSMessage>>>) -> Self {
-        WebSocketClient { url, tx, rx }
-    }
-
-    async fn try_connect(&self) -> Result<(), anyhow::Error> {
-        let (ws_stream, _) = connect_async(&self.url).await?;
-        let (mut sink, mut stream) = ws_stream.split();
-        let rx = self.rx.clone();
-        tokio::spawn(async move {
-            let mut rx = rx.lock().await;
-            while let Some(msg) = rx.recv().await {
-                println!("Sending message: {:?}", msg.message);
-                sink.send(tokio_tungstenite::tungstenite::Message::Binary(
-                    msg.message.into(),
-                ))
-                .await
-                .expect("Failed to send message");
-            }
-        });
-
-        let tx = self.tx.clone();
-        tokio::spawn(async move {
-            while let Some(msg) = stream.next().await {
-                let msg = msg.expect("Failed to receive message");
-                let msg = tungstenite::Message::Binary(msg.into_data());
-                let msg: Vec<u8> = msg.into_data().to_vec();
-                crate::decode_audio(&msg).expect("Failed to decode audio data");
-                tx.send(WSMessage { message: msg })
-                    .await
-                    .expect("Failed to send message");
-            }
-        });
-
-        println!("WebSocket handshake has been successfully completed");
-        Ok(())
-    }
-
-    async fn connect(&self) -> Result<(), anyhow::Error> {
-        match self.try_connect().await {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                println!("Failed to connect to WebSocket: {:?}", e);
-                self.reconnect().await
-            }
-        }
-    }
-
-    async fn reconnect(&self) -> anyhow::Result<()> {
-        let mut attempt = 0;
-        let max_attempts = 5;
-        let mut delay = Duration::from_secs(1);
-
-        while attempt < max_attempts {
-            if let Err(e) = self.try_connect().await {
-                eprintln!(
-                    "Failed to reconnect: {:?}. Attempt {}/{}",
-                    e,
-                    attempt + 1,
-                    max_attempts
-                );
-                attempt += 1;
-                sleep(delay).await;
-                delay *= 2; // Exponential backoff
-            } else {
-                println!(
-                    "Reconnected successfully on attempt {}/{}",
-                    attempt + 1,
-                    max_attempts
-                );
-                return Ok(());
-            }
-        }
-
-        Err(anyhow::anyhow!(
-            "Failed to reconnect after {} attempts",
-            max_attempts
-        ))
-    }
-}
-
-impl Client for WebSocketClient {
-    async fn connect(&self) -> Result<(), anyhow::Error> {
-        self.connect().await
-    }
-
-    async fn reconnect(&self) -> anyhow::Result<()> {
-        self.reconnect().await
-    }
-}
+use std::net::UdpSocket;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
 
 fn decode_audio(data: &[u8]) -> Result<Vec<f32>, Box<dyn std::error::Error>> {
     let mut output = Vec::new();
@@ -129,20 +18,27 @@ fn decode_audio(data: &[u8]) -> Result<Vec<f32>, Box<dyn std::error::Error>> {
 
 #[tokio::main]
 async fn main() {
-    let (tx_in, mut rx_in) = tokio::sync::mpsc::channel::<WSMessage>(100);
-    let (_tx_out, rx_out) = tokio::sync::mpsc::channel::<WSMessage>(100);
-    let url = "ws://localhost:8080";
     let (tx, rx) = std::sync::mpsc::channel::<Vec<f32>>();
+    let socket = UdpSocket::bind("0.0.0.0:8080").expect("Couldn't bind to address");
 
-    tokio::spawn(async move {
-        let ws_client = WebSocketClient::new(url.to_string(), tx_in, Arc::new(Mutex::new(rx_out)));
-        ws_client.connect().await.unwrap();
+    thread::spawn(move || {
+        let mut buf = [0; 4096];
+        loop {
+            match socket.recv(&mut buf) {
+                Ok(received) => {
+                    if let Ok(samples) = decode_audio(&buf[..received]) {
+                        tx.send(samples).unwrap();
+                    }
+                }
+                Err(e) => eprintln!("Failed to receive audio data: {}", e),
+            }
+        }
     });
 
     let host = cpal::default_host();
     let device = host
-        .default_input_device()
-        .expect("No input device available");
+        .default_output_device()
+        .expect("No output device available");
     let config = device.default_output_config().unwrap();
 
     println!(
@@ -153,16 +49,6 @@ async fn main() {
 
     println!("Receiving audio...");
 
-    tokio::spawn(async move {
-        while let Some(msg) = rx_in.recv().await {
-            println!("Received message: {:?}", msg.message);
-            if let Ok(samples) = decode_audio(&msg.message) {
-                println!("Received {} samples", samples.len());
-                tx.send(samples).unwrap();
-            }
-        }
-    });
-
     let stream = device
         .build_output_stream(
             &config.clone().into(),
@@ -171,7 +57,6 @@ async fn main() {
                     Ok(data) => data,
                     Err(_) => return,
                 };
-                println!("Playing {} samples", data.len());
                 for (output_sample, input_sample) in output.iter_mut().zip(data.iter()) {
                     *output_sample = *input_sample;
                 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use aprocess::ws;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use tokio::sync::mpsc;
+use std::net::UdpSocket;
 
 fn encode_audio(data: &[f32]) -> Result<Vec<u8>, Box<dyn std::error::Error + Send + Sync>> {
     let mut encoded = Vec::new();
@@ -25,39 +24,18 @@ async fn main() {
         config
     );
 
-    let (tx, audio_rx) = std::sync::mpsc::channel::<Vec<u8>>();
-    let (txin, audio_rxin) = std::sync::mpsc::channel::<Vec<f32>>();
-    tokio::spawn(async move {
-        ws::run(audio_rx).await;
-    });
-
-    tokio::spawn(async move {
-        loop {
-            match audio_rxin.recv() {
-                Ok(data) => {
-                    // println!("Received {} samples", data.len());
-                    let data = encode_audio(&data).unwrap();
-                    // println!("Encoded {} bytes : 22", data.len());
-                    if let Err(e) = tx.send(data) {
-                        eprintln!("Failed to receive audio data: {}", e);
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Failed to receive audio data: {}", e);
-                }
-            }
-        }
-    });
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("Couldn't bind to address");
+    socket.connect("127.0.0.1:8080").expect("Couldn't connect to server");
 
     let stream = device
         .build_input_stream(
             &config.into(),
             move |data: &[f32], _: &cpal::InputCallbackInfo| {
                 let data = data.to_vec();
-                let txin = txin.clone();
+                let socket = socket.try_clone().expect("Failed to clone socket");
                 std::thread::spawn(move || {
-                    // println!("Received {} samples 11", data.len());
-                    if let Err(e) = txin.send(data) {
+                    let encoded_data = encode_audio(&data).unwrap();
+                    if let Err(e) = socket.send(&encoded_data) {
                         eprintln!("Failed to send audio data: {}", e);
                     }
                 });
@@ -68,6 +46,6 @@ async fn main() {
         .unwrap();
 
     stream.play().unwrap();
-    println!("Streaming audio to");
-    tokio::time::sleep(tokio::time::Duration::from_secs(1000)).await; // Stream for 5 seconds
+    println!("Streaming audio to UDP server");
+    tokio::time::sleep(tokio::time::Duration::from_secs(1000)).await; // Stream for 1000 seconds
 }
